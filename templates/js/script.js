800f7ee1249576e9900b2018637fd6df8a6de303
Goodluck Alphaxard <133944511+Alphaxardjr@users.noreply.github.com>
2024-05-04 16:34:39 +0300

    Update script.js

--- templates/js/script.js
+++ templates/js/script.js
@@ -1,3 +1,51 @@
+ // ===== JS index.html ========
+
+
+// == dropdown menu ==
+
+document.addEventListener('click', function(e) {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]");
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+    });
+});
+
+
+
+
+
+// ======  navigations ==========//
+
+const barEl=document.querySelector('#sidebar')
+const toggleBtn=document.querySelector('.mobile-nav-toggle')
+
+toggleBtn.addEventListener('click',function(){
+    const barVisible=barEl.getAttribute("data-visible")
+    const toggleEpanded=toggleBtn.getAttribute("aria-expanded")
+    if(barVisible==="true"){
+        toggleBtn.setAttribute("aria-expanded",false)
+        barEl.setAttribute("data-visible",false)
+    } else{
+        toggleBtn.setAttribute("aria-expanded",true)
+        barEl.setAttribute("data-visible",true)
+    }
+
+})
+
+
+
+
+
+
 // Define all available items and their base prices
 const items = {
     'Sukuma Wiki': { basePrice: 30, price: 30 },
